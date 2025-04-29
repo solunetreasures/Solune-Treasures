bece35b7b2198fe749c651c7789f32086ec5d721
solunetreasures <arccollectables1@gmail.com>
2025-04-29 13:13:37 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
+import React from 'react';
 import './App.css';
-import logo from './solune_logo_header.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <img src={logo} alt="Solune Treasures" className="logo" />
+      <header className="App-header">
+        <h1>Solune Treasures</h1>
+        <p>A place where timeless pieces find new stories.</p>
       </header>
-      <main>
-        <h2>Welcome to Solune Treasures</h2>
-        <p>Your home for meaningful antiques and collectables.</p>
-      </main>
     </div>
   );
 }
